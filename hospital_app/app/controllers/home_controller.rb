40baa90cd58966a2ea281ce6890a9f6701d14a19
shikha805 <shikhaaggarwal805@gmail.com>
2016-10-04 18:13:10 +0530

    Update home_controller.rb

--- hospital_app/app/controllers/home_controller.rb
+++ hospital_app/app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def homepage
 
-    @patient=Patient.all;
-    @patient_limit_3=Patient.limit(3)
-    @physicianid=Physician.id
-    @physician_name=Physician.name
-    @ajay =Appointment.find_or_create_by(first_name: 'Ajay')
+    @patient = Patient.all;
+    @patient_limit_3 = Patient.limit(3)
+    @physicianid = Physician.id
+    @physician_name = Physician.name
+    @ajay = Appointment.find_or_create_by(first_name: 'Ajay')
   end
 end
